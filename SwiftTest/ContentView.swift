5a4df1be085f2da7e4439e94967369d4386659e1
hiromu <hiromureve1522@icloud.com>
2024-02-29 21:33:08 +0900

    Initial Commit

---
+++ SwiftTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTest
+//
+//  Created by 田中大夢 on 2024/02/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by 田中大夢 on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
