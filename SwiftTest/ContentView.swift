1e2678a1a9c9763f043b7c3dfc9798154c762823
hiromu <hiromureve1522@icloud.com>
2024-03-04 07:41:16 +0900

    Xcode上での練習

--- SwiftTest/ContentView.swift
+++ SwiftTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("ブランチでコマンド練習")
             Text("hiromu-v1ブランチ")
         }
         .padding()
