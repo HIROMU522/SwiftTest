dd595ccdb89244874a12291e6729cadaa9c4b3fb
hiromu <hiromureve1522@icloud.com>
2024-03-03 23:25:51 +0900

    Tesrコミット

--- SwiftTest/ContentView.swift
+++ SwiftTest/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("Practice")
+            Text("Practice1")
+            Text("Test")
         }
         .padding()
     }
